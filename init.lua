e9604bf44f261748f4cdd8a4a17d19993c60c144
Aweptimum <40273116+Aweptimum@users.noreply.github.com>
2021-05-14 12:55:34 -0400

    Still need relative require it seems

--- init.lua
+++ init.lua
@@ -9,6 +9,5 @@ function _Require_relative(PATH, file)
 	return require(table.concat({path, file}, "."))
 end
 
---local Dewall = _Require_relative(...,'DeWallua')
-local Dewall = require 'DeWallua'
+local Dewall = _Require_relative(...,'DeWallua')
 return Dewall
