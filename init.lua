4a88261b70b5b939312fac87d2ac3b1688a33f4b
Aweptimum <40273116+Aweptimum@users.noreply.github.com>
2021-09-25 11:56:34 -0400

    Update _Require_Relative

--- init.lua
+++ init.lua
@@ -1,11 +1,16 @@
 -- Relative require in same directory
 -- Global, because I don't want to declare it in every. single. file.
-function _Require_relative(PATH, file)
+function _Require_relative(PATH, file, up)
+    up = up or 0
+    print('up: '..up)
     local path, match
     path, _     = ( (PATH):gsub("\\",".") ):gsub("/",".")
     path, match = path:gsub("(.*)%..*$", "%1" )
+    for i = 1, up do
+        path, match = path:gsub("%.(%w+)$", '')
+    end
     --path = match == 0 and '.'..file or path
-    print('path: '..table.concat({path, file}, "."))
+    --print('path: '..table.concat({path, file}, "."))
 	return require(table.concat({path, file}, "."))
 end
 
