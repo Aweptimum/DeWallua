cb28ee519c1e57f370fa851f88386444cb53032c
Aweptimum <40273116+Aweptimum@users.noreply.github.com>
2021-09-26 16:51:10 -0400

    Remove print

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@
 -- Global, because I don't want to declare it in every. single. file.
 function _Require_relative(PATH, file, up)
     up = up or 0
-    print('up: '..up)
     local path, match
     path, _     = ( (PATH):gsub("\\",".") ):gsub("/",".")
     path, match = path:gsub("(.*)%..*$", "%1" )
